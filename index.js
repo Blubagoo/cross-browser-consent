bf3522a5c21edf020388eaccad1b3af152632151
Jim Tisdale <blubagoowenischinchilla@gmail.com>
2023-02-17 18:12:39 -0500

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const {google} = require('googleapis');
-
+var cors = require('cors')
 const app = express()
 const port = 8080
 
+app.use(cors())
 app.use(express.json())
 
 
